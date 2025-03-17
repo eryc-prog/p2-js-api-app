dcb93aaded49109617bbb0d77efb108f92ad3cc7
Eric Encarnacion <encarnacionerc@gmail.com>
2025-03-17 15:40:31 +0800

    Feat: Vanilla Javascript App

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
-require("dotenv").config();
-const express = require("express");
-const cors = require("cors");
+// require("dotenv").config();
+// const express = require("express");
+// const cors = require("cors");
 
-const app = express();
-app.use(cors());
+// const app = express();
+// app.use(cors());
 
-app.get("/api/key", (req, res) => {
-  res.json({ apiKey: process.env.API_KEY });
-});
+// app.get("/api/key", (req, res) => {
+//   res.json({ apiKey: process.env.API_KEY });
+// });
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
